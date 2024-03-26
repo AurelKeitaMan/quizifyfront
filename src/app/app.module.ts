cf66d81e1e4af53d10bba83002a004a372eaf634
manueitsmy <manueitsmy@gmail.com>
2024-03-26 13:45:11 +0100

    Page play en cours 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -38,7 +39,9 @@ import { IconComponent } from './components/icon/icon.component';
     FormCategoryComponent,
     IconComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule,
+    AppRoutingModule,
+    FormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
