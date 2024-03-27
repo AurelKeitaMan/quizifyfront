83012dba082479db3ccf287ca7a2c23321633715
KeitaroBlaster <rskp95@yahoo.fr>
2024-03-27 14:14:06 +0100

    1er commit stats

--- src/app/pages/my-stats-page/my-stats-page.component.ts
+++ src/app/pages/my-stats-page/my-stats-page.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { StatsService } from 'src/app/services/stats.service';
 
 @Component({
   selector: 'app-my-stats-page',
   templateUrl: './my-stats-page.component.html',
   styleUrls: ['./my-stats-page.component.css']
 })
-export class MyStatsPageComponent {
+export class MyStatsPageComponent implements OnInit {
+  nombrePartieJouee: number = 0;
+  totalScore: number = 0;
+  scoreParCategorie: { categorie: string, score: number }[] = [];
+
+
+  constructor(private statsService: StatsService){}
+
+
+  ngOnInit(): void {
+    this.nombrePartieJouee = this.statsService.nombrePartieJouee;
+    this.totalScore = this.statsService.totalScore;
+    this.scoreParCategorie = this.statsService.scoreParCategorie;
+  }
 
 }
