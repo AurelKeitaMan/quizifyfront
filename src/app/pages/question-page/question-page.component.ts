6dd881548e9bdf6625f1389fc3c188f8228f32e8
manueitsmy <manueitsmy@gmail.com>
2024-03-27 10:12:45 +0100

    page question 2eme commit

--- src/app/pages/question-page/question-page.component.ts
+++ src/app/pages/question-page/question-page.component.ts
@@ -6,5 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./question-page.component.css']
 })
 export class QuestionPageComponent {
+  selectedOption: string | null = null;
 
+  constructor() { }
+
+  selectOption(option: string) {
+    this.selectedOption = option;
+  }
+
+  valider() {
+    if (this.selectedOption) {
+      console.log("Option sélectionnée :", this.selectedOption);
+      // Ajoutez ici la logique pour gérer la sélection, par exemple, envoyer la réponse au serveur, etc.
+    } else {
+      console.log("Aucune option sélectionnée");
+      // Ajoutez ici la logique pour gérer le cas où aucune option n'est sélectionnée
+    }
+  }
 }
